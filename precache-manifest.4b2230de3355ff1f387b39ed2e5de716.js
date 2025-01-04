c2e6a0cb05ba28d2b57b3e3b9c03ef54d6bd4c33
Aleksandr Chikurov <alchikurov@gmail.com>
2025-01-04 18:21:09 +0300

    Updates

---
+++ precache-manifest.4b2230de3355ff1f387b39ed2e5de716.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bad987aa9946254a6fd8c2baa9beb097",
+    "url": "/notion_grid/index.html"
+  },
+  {
+    "revision": "ff0e8ea68df696ecc5d8",
+    "url": "/notion_grid/static/css/main.a3670ca7.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/notion_grid/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "8e3a127b3eb410b6d841",
+    "url": "/notion_grid/static/js/3.445915d0.chunk.js"
+  },
+  {
+    "revision": "ff0e8ea68df696ecc5d8",
+    "url": "/notion_grid/static/js/main.e1ccc81d.chunk.js"
+  },
+  {
+    "revision": "bb3dbcb2b9eba26218da",
+    "url": "/notion_grid/static/js/runtime~main.46d7626c.js"
+  }
+]);
